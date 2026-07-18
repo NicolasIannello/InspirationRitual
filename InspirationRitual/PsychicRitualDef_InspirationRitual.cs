d18bf3f22eba1a70f92bd258ee7db6886d518ede
NicolasIannello <nicolas.iannello99@gmail.com>
2026-07-18 18:48:19 -0300

    1.0

--- InspirationRitual/PsychicRitualDef_InspirationRitual.cs
+++ InspirationRitual/PsychicRitualDef_InspirationRitual.cs
@@ -1,5 +1,6 @@
 ﻿using RimWorld;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using Verse;
 using Verse.AI.Group;
@@ -9,6 +10,8 @@ namespace InspirationRitual
     public class PsychicRitualDef_InspirationRitual : PsychicRitualDef_InvocationCircle
     {
         public SimpleCurve deathChanceFromQualityCurve;
+        public static Dictionary<string, List<InspirationDef>> inspiphagyList;
+        private static List<SkillRecord> tmpCandidateSkills = new List<SkillRecord>();
 
         public override List<PsychicRitualToil> CreateToils(PsychicRitual psychicRitual, PsychicRitualGraph parent)
         {
@@ -20,7 +23,86 @@ namespace InspirationRitual
 
         public override TaggedString OutcomeDescription(FloatRange qualityRange, string qualityNumber, PsychicRitualRoleAssignments assignments)
         {
-            return outcomeDescription.Formatted(Mathf.FloorToInt(deathChanceFromQualityCurve.Evaluate(qualityRange.min)));
+            TaggedString result = outcomeDescription.Formatted(DeathChance(qualityRange.min));
+            if (inspiphagyList == null) PopulateInspiphagy();
+
+            Pawn pawn = assignments.FirstAssignedPawn(InvokerRole);
+            Pawn pawn2 = assignments.FirstAssignedPawn(TargetRole);
+            if (pawn == null || pawn2 == null) return result;
+
+            List<InspirationDef> inspiphagyInspiration = GetInspiphagyInspiration(pawn, pawn2);
+            string inspirations="";
+            for (int i = 0; i < inspiphagyInspiration.Count; i++)
+            {
+                inspirations += inspiphagyInspiration[i].LabelCap;
+                if (i != inspiphagyInspiration.Count - 1) inspirations += " / ";
+            }
+
+            return result+= "\n\nInspiration: " + inspirations;   
+        }
+
+        public static void PopulateInspiphagy()
+        {
+            inspiphagyList = new Dictionary<string, List<InspirationDef>>();
+            foreach (var d in DefDatabase<InspirationDef>.AllDefs)
+            {
+                if (d.associatedSkills != null)
+                {
+                    foreach (var item in d.associatedSkills)
+                    {
+                        if (!inspiphagyList.ContainsKey(item.defName))
+                        {
+                            inspiphagyList[item.defName] = new List<InspirationDef>();
+                        }
+                        inspiphagyList[item.defName].Add(d);
+                    }
+                }
+                else
+                {
+                    if (!inspiphagyList.ContainsKey("Default"))
+                    {
+                        inspiphagyList["Default"] = new List<InspirationDef>();
+                    }
+                    inspiphagyList["Default"].Add(d);
+                }
+            }
+        }
+
+        public int DeathChance(float quality)
+        {
+            return Mathf.FloorToInt(deathChanceFromQualityCurve.Evaluate(quality));
+        }
+
+        public static List<InspirationDef> GetInspiphagyInspiration(Pawn invoker, Pawn target)
+        {
+            IOrderedEnumerable<SkillRecord> orderedEnumerable = from s in target.skills.skills
+                                                                where !s.TotallyDisabled && !invoker.skills.GetSkill(s.def).TotallyDisabled && s.Level>2
+                                                                orderby s.XpTotalEarned descending
+                                                                select s;
+            
+            if (!orderedEnumerable.Any()) return inspiphagyList["Default"];
+            tmpCandidateSkills.Clear();
+
+            SkillRecord skillRecord = orderedEnumerable.First();
+            foreach (SkillRecord item in orderedEnumerable)
+            {
+                if (item == skillRecord)
+                {
+                    tmpCandidateSkills.Add(item);
+                }
+                else if (Mathf.Abs(item.XpTotalEarned - skillRecord.XpTotalEarned) < 0.01f)
+                {
+                    tmpCandidateSkills.Add(item);
+                }
+            }
+
+            SkillDef def = null;
+            foreach (SkillRecord tmpCandidateSkill in tmpCandidateSkills)
+            {
+                if (def == null && invoker.skills.GetSkill(tmpCandidateSkill.def).Level > 2) def = tmpCandidateSkill.def;
+            }
+
+            return def!=null ? inspiphagyList.ContainsKey(def.defName) ? inspiphagyList[def.defName] : inspiphagyList["Default"] : inspiphagyList["Default"];
         }
     }
 }
--- InspirationRitual/PsychicRitualToil_InspirationRitual.cs
+++ InspirationRitual/PsychicRitualToil_InspirationRitual.cs
@@ -1,5 +1,4 @@
 ﻿using RimWorld;
-using UnityEngine;
 using Verse;
 using Verse.AI.Group;
 
@@ -8,9 +7,7 @@ namespace InspirationRitual
     public class PsychicRitualToil_InspirationRitual : PsychicRitualToil
     {
         public PsychicRitualRoleDef invokerRole;
-
         public PsychicRitualRoleDef targetRole;
-
         public int comaDurationTicks = 60000 * 7;
 
         protected PsychicRitualToil_InspirationRitual() { }
@@ -24,25 +21,34 @@ namespace InspirationRitual
         public override void Start(PsychicRitual psychicRitual, PsychicRitualGraph parent)
         {
             base.Start(psychicRitual, parent);
-            var def = (PsychicRitualDef_InspirationRitual)psychicRitual.def;
             var invoker = psychicRitual.assignments.FirstAssignedPawn(invokerRole);
             var target = psychicRitual.assignments.FirstAssignedPawn(targetRole);
-            if (invoker == null || target == null)
-            {
-                return;
-            }
+            if (invoker == null || target == null) return;
             ApplyOutcome(psychicRitual, invoker, target);
         }
 
         private void ApplyOutcome(PsychicRitual psychicRitual, Pawn invoker, Pawn target)
         {
-            var hediff = HediffMaker.MakeHediff(HediffDefOf.RegenerationComa, target);
+            if (PsychicRitualDef_InspirationRitual.inspiphagyList == null) PsychicRitualDef_InspirationRitual.PopulateInspiphagy();
+
+            var hediff = HediffMaker.MakeHediff(HediffDefOf.DarkPsychicShock, target);
             hediff.TryGetComp<Verse.HediffComp_Disappears>()?.SetDuration(comaDurationTicks);
             target.health.AddHediff(hediff);
 
-            TaggedString text = "InspirationRitual.Complete".Translate(invoker.Named("INVOKER"), psychicRitual.def.Named("RITUAL"), target.Named("TARGET"));
-                //text += "\n\n" + "InhumanizeMentalBreak".Translate(target.Named("TARGET"));
-            
+            InspirationDef grantedInspiration = PsychicRitualDef_InspirationRitual.GetInspiphagyInspiration(invoker, target).RandomElement();
+            invoker.mindState.inspirationHandler.TryStartInspiration(grantedInspiration, psychicRitual.def.label+" Ritual");
+            TaggedString text = "InspirationRitual.Complete".Translate(invoker.Named("INVOKER"), psychicRitual.def.Named("RITUAL"), grantedInspiration.LabelCap);
+
+            int deatChance = (psychicRitual.def as PsychicRitualDef_InspirationRitual)?.DeathChance(psychicRitual.power) ?? 0;
+            if (Rand.Chance(deatChance / 100f))
+            {
+                target.Kill(null);
+                text+= "\n\n" + "InspirationRitual.Death".Translate(target.Named("INVOKER"));
+            }
+            else
+            {
+                text += "\n\n" + "InspirationRitual.Coma".Translate(target.Named("INVOKER"));
+            }
             Find.LetterStack.ReceiveLetter("PsychicRitualCompleteLabel".Translate(psychicRitual.def.label), text, LetterDefOf.NeutralEvent, new LookTargets(invoker, target));
         }
 
