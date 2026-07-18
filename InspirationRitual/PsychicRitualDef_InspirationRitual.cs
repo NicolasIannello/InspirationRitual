70ca993c6d2b2530b26be837301b972821b70b9d
NicolasIannello <nicolas.iannello99@gmail.com>
2026-07-17 21:15:20 -0300

    initial

---
+++ InspirationRitual/PsychicRitualDef_InspirationRitual.cs
@@ -0,0 +1,26 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+using Verse.AI.Group;
+
+namespace InspirationRitual
+{
+    public class PsychicRitualDef_InspirationRitual : PsychicRitualDef_InvocationCircle
+    {
+        public SimpleCurve deathChanceFromQualityCurve;
+
+        public override List<PsychicRitualToil> CreateToils(PsychicRitual psychicRitual, PsychicRitualGraph parent)
+        {
+            var list = base.CreateToils(psychicRitual, parent);
+            list.Add(new PsychicRitualToil_InspirationRitual(InvokerRole, TargetRole));
+            list.Add(new PsychicRitualToil_TargetCleanup(InvokerRole, TargetRole));
+            return list;
+        }
+
+        public override TaggedString OutcomeDescription(FloatRange qualityRange, string qualityNumber, PsychicRitualRoleAssignments assignments)
+        {
+            return outcomeDescription.Formatted(Mathf.FloorToInt(deathChanceFromQualityCurve.Evaluate(qualityRange.min)));
+        }
+    }
+}
---
+++ InspirationRitual/PsychicRitualToil_InspirationRitual.cs
@@ -0,0 +1,57 @@
+﻿using RimWorld;
+using UnityEngine;
+using Verse;
+using Verse.AI.Group;
+
+namespace InspirationRitual
+{
+    public class PsychicRitualToil_InspirationRitual : PsychicRitualToil
+    {
+        public PsychicRitualRoleDef invokerRole;
+
+        public PsychicRitualRoleDef targetRole;
+
+        public int comaDurationTicks = 60000 * 7;
+
+        protected PsychicRitualToil_InspirationRitual() { }
+
+        public PsychicRitualToil_InspirationRitual(PsychicRitualRoleDef invokerRole, PsychicRitualRoleDef targetRole)
+        {
+            this.invokerRole = invokerRole;
+            this.targetRole = targetRole;
+        }
+
+        public override void Start(PsychicRitual psychicRitual, PsychicRitualGraph parent)
+        {
+            base.Start(psychicRitual, parent);
+            var def = (PsychicRitualDef_InspirationRitual)psychicRitual.def;
+            var invoker = psychicRitual.assignments.FirstAssignedPawn(invokerRole);
+            var target = psychicRitual.assignments.FirstAssignedPawn(targetRole);
+            if (invoker == null || target == null)
+            {
+                return;
+            }
+            ApplyOutcome(psychicRitual, invoker, target);
+        }
+
+        private void ApplyOutcome(PsychicRitual psychicRitual, Pawn invoker, Pawn target)
+        {
+            var hediff = HediffMaker.MakeHediff(HediffDefOf.RegenerationComa, target);
+            hediff.TryGetComp<Verse.HediffComp_Disappears>()?.SetDuration(comaDurationTicks);
+            target.health.AddHediff(hediff);
+
+            TaggedString text = "InspirationRitual.Complete".Translate(invoker.Named("INVOKER"), psychicRitual.def.Named("RITUAL"), target.Named("TARGET"));
+                //text += "\n\n" + "InhumanizeMentalBreak".Translate(target.Named("TARGET"));
+            
+            Find.LetterStack.ReceiveLetter("PsychicRitualCompleteLabel".Translate(psychicRitual.def.label), text, LetterDefOf.NeutralEvent, new LookTargets(invoker, target));
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Defs.Look(ref invokerRole, "invokerRole");
+            Scribe_Defs.Look(ref targetRole, "targetRole");
+            Scribe_Values.Look(ref comaDurationTicks, "comaDurationTicks");
+        }
+    }
+}
